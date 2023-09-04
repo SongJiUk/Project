3723129ea4f897b49956ec6a42e9e1de7f3649b5
Jiuk <wldnr1014@naver.com>
2023-09-04 19:20:45 +0900

    캐릭터 이동 부드럽게, 애니메이션 추가

--- Project/Assets/5. Script/_Player/Player.cs
+++ Project/Assets/5. Script/_Player/Player.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class Player : Singleton<Player>
@@ -16,6 +17,8 @@ public class Player : Singleton<Player>
     public float HP { get { return hp; } }
 
     [SerializeField] Animator anim;
+    [SerializeField] NavMeshAgent nav;
+    Vector3 moveVelocity = Vector3.zero;
     bool isClickLeftMouse;
     Vector3 MoveDirection = Vector3.zero;
 
@@ -56,17 +59,27 @@ public class Player : Singleton<Player>
 
     public void OnRightClick(InputAction.CallbackContext context)
     {
-        
-        //Vector2 input = context.ReadValue<Vector2>();
-        //MoveDirection = new Vector3(input.x, 0f, input.y);
-        //Debug.Log(MoveDirection);
-
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            nav.SetDestination(hit.point);
+        }
     }
 
-    
+
     private void FixedUpdate()
     {
-        transform.position += MoveDirection.normalized * Time.deltaTime * 3f;
+        CheckPlaterVelocity();
+
+        //키보드
+        //transform.position += MoveDirection.normalized * Time.deltaTime * 3f;
+    }
+
+    public void CheckPlaterVelocity()
+    {
+
+        float MoveAnim = nav.remainingDistance < nav.stoppingDistance ? 0f : 1f;
+        anim.SetFloat("Velocity", MoveAnim, 0.5f, Time.deltaTime);
     }
 
 }
