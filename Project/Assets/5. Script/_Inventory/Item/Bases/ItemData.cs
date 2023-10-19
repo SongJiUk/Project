50184980c1f8af6e346bba9ccc8822f33364a8d1
YU <suk99yu66@gmail.com>
2023-10-19 21:13:57 +0900

    장비창이랑 사람 장비 연결 하는 중

---
+++ Project/Assets/5. Script/_Inventory/Item/Bases/ItemData.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+/*
+    [상속 구조]
+
+    ItemData(abstract)
+        - CountableItemData(abstract)
+            - PortionItemData
+        - EquipmentItemData(abstract)
+            - WeaponItemData
+            - ArmorItemData
+
+*/
+
+public enum Ratings
+{
+    Common,
+    Rare,
+    Unique,
+    Legendary,
+    Mythic
+}
+
+public abstract class ItemData : ScriptableObject
+{
+    public int ItemCode => _itemcode;
+    public string Name => _name;
+    public string Tooltip => _tooltip;
+    public Sprite IconSprite => _iconSprite;
+    public Ratings Rating => _rating;
+
+    [SerializeField] private Ratings _rating;
+    [SerializeField] private int _itemcode;
+    [SerializeField] private string _name;    // 아이템 이름
+    [Multiline]
+    [SerializeField] private string _tooltip; // 아이템 설명
+    [SerializeField] private Sprite _iconSprite; // 아이템 아이콘
+    [SerializeField] private GameObject _dropItemPrefab; // 바닥에 떨어질 때 생성할 프리팹
+    [SerializeField] private GameObject _SettingItem; // 착용 아이템 
+
+    /// <summary> 타입에 맞는 새로운 아이템 생성 </summary>
+    public abstract Item CreateItem();
+}
---
+++ Project/Assets/5. Script/_Inventory/Item/EquipmentItemData/PlayerEquipmentItemData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEquipmentItemData : MonoBehaviour
+{
+    [SerializeField] public bool _male;
+
+    public Types _type;
+
+    public ClassPrivateItems _classPrivateItems;
+
+    public int _num;
+
+
+
+    
+}
