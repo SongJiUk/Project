c369e65418a5f70c36c39368bbf0d6cf0a2af9ab
ChuSounghyen <fillitup04@gmail.com>
2023-10-25 16:07:59 +0900

    이동하는 NPC

--- Project/Assets/5. Script/NPC/MoveNPC.cs
+++ Project/Assets/5. Script/NPC/MoveNPC.cs
@@ -10,17 +10,30 @@ public class MoveNPC : MonoBehaviour
     float moveSpeed = 2.0f;
 
     int currentWaypoint = 0;
+    bool turn = true;
+
+    Animator ani;
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
 
     private void Update()
     {
+        ani.SetTrigger("RunStart");
         if (currentWaypoint < waypoints.Length)
         {
             Vector3 targetPosition = waypoints[currentWaypoint].position;
+            if (turn)
+            {
+                transform.LookAt(targetPosition);
+                turn = false;
+            }
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-
             if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
             {
                 currentWaypoint++;
+                turn = true;
             }
         }
         else
